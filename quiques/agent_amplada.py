9fb072f5b92abac1fe5ee73874732b9b58518363
AdondeBoy <106172989+AdondeBoy@users.noreply.github.com>
2023-10-03 13:23:14 +0200

    Empiezo lo de gallinas y lobos

--- quiques/agent_amplada.py
+++ quiques/agent_amplada.py
@@ -10,7 +10,23 @@ class BarcaAmplada(Barca):
         self.__tancats = None
         self.__accions = None
 
+    def cerca_general(self):
+        X = Estat(0,3,3,)
+        self.__oberts = [X]
+        self.__tancats = []
+        while self.__oberts:
+            X = self.__oberts.pop(0)
+            if X.es_meta():
+                return True, self.__oberts
+            else:
+                fills = X.genera_fill()
+                # recorrer fills y remove los malos
+                self.__tancats.append(X)
+                self.__oberts += fills
+        return False
+
+
     def actua(
             self, percepcio: entorn.Percepcio
     ) -> entorn.Accio | tuple[entorn.Accio, object]:
-        pass
+        return do()
